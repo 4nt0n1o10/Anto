f7407433583b12778b56b16b0fa3d51f13f91a42
4nt0n1o10 <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-15 15:18:59 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("\033[1m\033[31mLE VELINEEEEEEEEEEE\n\33[34m");
+
+    int l = 0;
+    float o = 3.4672515;
+    char r = 'r';
+   
+    printf("%d\n", l);
+    printf("%f\n", o);
+    printf("%c\n", r);
+
+    return 0;
+}
+
