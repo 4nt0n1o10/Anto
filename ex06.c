37b9e013014676df9c03b5064aed28fb38122756
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-16 13:03:06 +0100

    Add files via upload

---
+++ ex06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+ 
+    float a;
+    float b;
+    float c;
+   
+    printf("Inserisci tre numeri. Anallizerò i dati e capirò se sono in succesione aritmetica.\n\33[34m");
+     scanf(" %f", &a);
+     scanf(" %f", &b);
+     scanf(" %f", &c);
+
+    if (a - b == b - c ) {
+        printf("\33[32mI numeri forniti sono in progressione.\n\33[32m\33[34m");
+    }
+    else {
+        printf("\33[31m I numeri non sono in succesione aritmetica\n\33[31m\33[34m");    
+    }
+
+    return 0;
+}
