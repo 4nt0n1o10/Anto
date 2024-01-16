355b0a395ee270d7e8cda495063cc6cf293eb74d
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-16 10:56:38 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+    int Io; 
+    int Tu;
+
+    printf("quanto salti lontano?\n\33[34m");
+    scanf(" %d", &Io);
+    scanf(" %d", &Tu);
+    if (Tu == 120) {
+        printf("saltiamo alla stessa distanza\n");
+    }
+    else if (Tu <= 119) {
+        printf("salto più lontano Io\n");
+    }
+    else{
+        printf("salti più lontano Tu\n");
+    }
+
+    return 0;
+}
+    
