d93de0889d9d93fb7615aaaeea11b76badce9ac2
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-17 12:48:40 +0100

    Add files via upload

--- ex12.c
+++ ex12.c
@@ -3,26 +3,20 @@
 void arionCogn() {
     float a;
     float b;
-    float c;
 
-    printf("\33[34mIserisci un numero, così che io possa trovarne la radice.\n");
+    printf("\33[34mIserisci un numero:");
     scanf(" %f", &a);
-    b = a / 2;
-    c = 0;
-
-    while (b != c)
-    {
-        c = b;
-        b = (a / c + c) / 2;        
-    }
-
-    printf("\33[33mLa radice del tuo numero %f è %f\n\33[34m", a, b);
+    b = a * a * a;
+    printf("Il cubo del tuo numero è %f\n", b);
 }
 
-int main(){
-    arionCogn();
-    arionCogn();
-    arionCogn();
-    arionCogn();
+int main() {
+    int i = 0;
+
+    while(1 < 4)
+    {
+        arionCogn();
+        i ++;
+    }    
     return 0;
 }
