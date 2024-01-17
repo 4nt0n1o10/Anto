25e801afc8cf2b71d77649155e36f363c16905f9
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-17 10:00:00 +0100

    .

--- ex10.c
+++ ex10.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main(){
 
-int a;
+    int a;
 
-printf("\33[34mDimmi l'anno della tua nascita, vediamo se sei coetaneo o meno con il primo uomo andato sulla luna\n");
-scanf(" %d", &a);
+    printf("\33[34mDimmi l'anno della tua nascita, vediamo se sei coetaneo o meno con il primo uomo andato sulla luna\n");
+    scanf(" %d", &a);
 
-if (a == 1969) {
-    printf("\33[32mSei nato/a lo stesso anno in cui l'uomo andò sulla luna. Che culo, il tuo ascnedente lunare è del USA!\n\33[34m");
-}
+    if (a == 1969) {
+        printf("\33[32mSei nato/a lo stesso anno in cui l'uomo andò sulla luna. Che culo, il tuo ascnedente lunare è del USA!\n\33[34m");
+    }
 
-else if (a < 1969) {
-    printf("\33[33mSei nato/a prima di quando l'uomo andò sulla luna, esattamente %d anni prima. Non dovresti essere polvere ormai???\n\33[34m", 1969 - a);
-}
+    else if (a < 1969) {
+        printf("\33[33mSei nato/a prima di quando l'uomo andò sulla luna, esattamente %d anni prima. Non dovresti essere polvere ormai???\n\33[34m", 1969 - a);
+    }
 
-else {
-    printf("\33[31mSei nato/a dopo che l'uomo andò sulla luna, esattamente %d anni dopo. Dai, hai ancora tempo, magari sarai il prosimo ad andarci!\n\33[34m", a - 1969);
-}
+    else {
+        printf("\33[31mSei nato/a dopo che l'uomo andò sulla luna, esattamente %d anni dopo. Dai, hai ancora tempo, magari sarai il prosimo ad andarci!\n\33[34m", a - 1969);
+    }
 
-return 0;
+    return 0;
 
 }
