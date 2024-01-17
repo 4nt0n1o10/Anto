6f503b1143cf344de2c67b31552983d45ae31227
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-17 15:46:35 +0100

    Add files via upload

--- ex14.c
+++ ex14.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
         
 int main() {
-    int i =  100;
+    int i = 100;
 
     while (i > 0) 
     {
-       printf("%d\n", i - 2); 
+       printf("%d\n", i); 
        i = i - 2;
     }
         
