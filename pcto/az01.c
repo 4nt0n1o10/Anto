96c9303fd72ac21172c98ee8bd26ec21638cc8a2
Anto <156683608+4nt0n1o10@users.noreply.github.com>
2024-01-19 12:02:38 +0000

    COMPLETO

--- pcto/az01.c
+++ pcto/az01.c
@@ -1,83 +1,17 @@
 #include <stdio.h>
-void arra(char *a) 
+int checkNum(char *on) 
 {
+    int err = 0;
     int n = 0;
-    while (a[n] != '\0') 
+    while (on[n] != '\0' && err == 0) 
     {
-        if  (a[n] >= 48 &&  a[n]<= 57)
-        {
-
-        }
-        else 
-        {
+        if  (on[n] < 48 || on[n] > 57){
             printf("Inserisci un numero per favore, adesso riprova\n");
+            err = 1;
         }
         n++;
     }
-}
-void arrb(char *b) 
-{
-    int n = 0;
-    while (b[n] != '\0') 
-    {
-        if  (b[n] >= 48 &&  b[n]<= 57)
-        {
-
-        }
-        else 
-        {
-            printf("Inserisci un numero per favore, adesso riprova\n");
-        }
-        n++;
-    }
-}
-void arrc(char *c) 
-{
-    int n = 0;
-    while (c[n] != '\0') 
-    {
-        if  (c[n] >= 48 &&  c[n]<= 57)
-        {
-
-        }
-        else 
-        {
-            printf("Inserisci un numero per favore, adesso riprova\n");
-        }
-        n++;
-    }
-}
-void arrd(char *d) 
-{
-    int n = 0;
-    while (d[n] != '\0') 
-    {
-        if  (d[n] >= 48 &&  d[n]<= 57)
-        {
-
-        }
-        else 
-        {
-            printf("Inserisci un numero per favore, adesso riprova\n");
-        }
-        n++;
-    }
-}
-void arre(char *e) 
-{
-    int n = 0;
-    while (e[n] != '\0') 
-    {
-        if  (e[n] >= 48 && e[n]<= 57)
-        {
-
-        }
-        else 
-        {
-            printf("Inserisci un numero per favore, adesso riprova\n");
-        }
-        n++;
-    }
+    return (err);
 }
 
 int main(){
@@ -86,49 +20,89 @@ int main(){
     char c [30];
     char d [30];
     char e [30];
+    char f [30];
+    char g [30];
 
     //PARTE CHE dICE COME dEVI VESTIRTI
-    printf("In oltre, dato che farai serata con amici, ti consiglio di salvarti alcuni contatti (scrivi i numeri attacati fra loro):\n");
-    while()
+    printf("In oltre, dato che farai serata, ti consiglio di salvarti alcuni contatti (scrivi i numeri attacati fra loro):\n");
+    while(1)
     {
         printf("Mamma\n");
         scanf(" %s", a); 
-        arra(a);
-        if (1 == 1) 
+        if(checkNum(a) == 1)
         {
-            printf("Mamma %s\n", a);   
+            continue;
+        }
+        else 
+        {
+            printf("Mamma %s\n", a);
         }
         printf("Papà\n");
         scanf(" %s", b); 
-        arrb(b);
-        if (1 == 1)
+        if(checkNum(b) == 1)
+        {
+            continue ;
+        }
+        else 
         {
             printf("Papà %s\n", b);
         }
         printf("Sorella/Fratello\n");
         scanf(" %s", c);
-        arrc(c);
-        if (1 == 1)
+        if(checkNum(c) == 1)
+        {
+            continue ;
+        }
+        else 
         {
             printf("Sorella/Fratello %s\n", c);
         }
         printf("Nonna\n");
         scanf(" %s", d);
-        arrd(d);
-        if (1 == 1)
+        if(checkNum(d) == 1)
         {
-            printf("Nonna %s\n",d);
+            continue ;
+        }
+        else 
+        {
+            printf("Nonna %s\n", a);
         }
         printf("Amico\n");
         scanf(" %s", e);
-        arre(e);
-        if (1 == 1)
+        if(checkNum(e) == 1)
+        {
+            continue ;
+        }
+        else
         {
             printf("Amico %s\n", e);
+            break;
         }        
     }
-    
-    
+    printf("Ah, io bloccherei anche qualche numero ''proibito'', così per sicurezza:\n");
+    while 
+    {
+        printf("Ex fidanzata\n");
+        scanf(" %s", f); 
+        if(checkNum(f) == 1)
+        {
+            continue;
+        }
+        else 
+        {
+            printf("Ex %s\n", f);
+        }
+        printf("Persona verso cui provi rancore\n");
+        scanf(" %s", g); 
+        if(checkNum(g) == 1)
+        {
+            continue;
+        }
+        else 
+        {
+            printf("Persona verso cui provi rancore %s\n", g);
+        }
+    }
     
     return 0;
 }
